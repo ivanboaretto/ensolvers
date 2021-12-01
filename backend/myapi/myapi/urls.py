d1ba8511f7a11be7cdb059d9d42a3f0a052617e3
ivanboaretto <ivanboaretto17@gmail.com>
2021-11-30 22:27:06 -0300

    Endpoints

--- backend/myapi/myapi/urls.py
+++ backend/myapi/myapi/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from myapp import views
 urlpatterns = [
-    #path('admin/', admin.site.urls),
     path('todo', views.add_todo),
-    path('todo_list', views.get_todo_list)
+    path('todo_list', views.get_todo_list),
+    path('todo/<int:pk>',views.update_todo)
 ]
