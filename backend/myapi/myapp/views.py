59f01f9a3f424ff4810abead6b2cd5fec3051f47
ivanboaretto <ivanboaretto17@gmail.com>
2021-11-30 22:08:36 -0300

    Handlers for update and remove

--- backend/myapi/myapp/views.py
+++ backend/myapi/myapp/views.py
@@ -1,26 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
 from rest_framework import routers
-import json
-from myapp.models import ToDo
 from django.views.decorators.csrf import csrf_exempt
+from myapp.serializers import ToDoSerializer
+from myapp.models import ToDo
 
 @csrf_exempt 
 def get_todo_list(request):
     if request.method == 'GET':
-        todo_list = []
-        for todo in ToDo.objects.all():
-            todo_list.append({'description': todo.description, 'completed': todo.completed})
-    return HttpResponse(json.dumps(todo_list),content_type='text/json')
+        todo_list = ToDo.objects.all()
+        serializer = ToDoSerializer(todo_list,many=True)
+        return JsonResponse(serializer.data,safe=False)
+    return HttpResponse(status=404)
 
 @csrf_exempt 
 def add_todo(request):
     if request.method == 'POST':
-        request_body = json.loads(request.body)
-        todo = ToDo(description=request_body['description'],completed=request_body['completed'])
-        try:
-            todo.save()
-            response = json.dumps([{'Success': 'ToDo added successfully!'}])
-        except: 
-            response = json.dumps([{'Error': 'ToDo could not be added'}])
-    return HttpResponse(response,content_type='text/json')
+        data = JSONParser().parse(request)
+        serializer = ToDoSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    return HttpResponse(status=404)
+    
+@csrf_exempt
+def update_todo(request,pk):
+    try: 
+        todo = ToDo.objects.get(pk=pk)
+    except ToDo.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ToDoSerializer(todo, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        todo.delete()
+        return HttpResponse(status=204)
